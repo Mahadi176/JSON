d2a212f7c5dc429767ca9db165a1764b4f40f593
Mahadi176 <mahadi210176@gmail.com>
2026-03-03 02:34:07 +0600

    push

--- json.js
+++ json.js
@@ -1,14 +1,35 @@
-const person = {
-    name : "babul",
-    father : "abul",
-    place : "kabul",
-    friend :"Rahul"
-}
+// const person = {
+//     name : "babul",
+//     father : "abul",
+//     place : "kabul",
+//     friend :"Rahul"
+// }
 
-console.log(person, typeof person)
+// console.log(person, typeof person)
 
-const personJson = JSON.stringify(person)
-console.log(personJson, typeof personJson)
+// const personJson = JSON.stringify(person)
+// console.log(personJson, typeof personJson)
 
-const parseJson = JSON.parse(personJson)
-console.log(parseJson, typeof parseJson)
+// const parseJson = JSON.parse(personJson)
+// console.log(parseJson, typeof parseJson)
+
+
+
+const loadData = () =>{
+          fetch('https://jsonplaceholder.typicode.com/todos/1')
+          .then(res => res.json())
+                .then(json => console.log(json))
+      }
+
+      
+      const loadPost = () => {
+        const url = 'https://jsonplaceholder.typicode.com/posts'
+        fetch(url)
+        .then(res => res.json())
+        .then(json => displayPost(json))
+
+      }
+
+      const displayPost = (posts) =>{
+            posts.forEach(post => console.log(post))
+      }
